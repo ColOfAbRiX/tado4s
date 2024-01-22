980d581eddd5dad32db216c4284ac6fca2102aea
Fabrizio Colonna <colofabrix@tin.it>
2024-01-22 15:46:42 +0000

    Saving before messing

---
+++ src/test/scala/com/colofabrix/scala/tado4s/Tado4s.scala
@@ -0,0 +1,8 @@
+package com.colofabrix.scala.tado4s
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class Tado4sSpec extends AnyFlatSpec with Matchers:
+
+  "Tado4s" should "work" in {}
